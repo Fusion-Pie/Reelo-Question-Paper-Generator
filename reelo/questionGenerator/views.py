5e0d6b70e8149431dbd34791864f65fb553bc62a
Fusion-Pie <piyushhatewar@gmail.com>
2023-11-22 18:03:46 +0530

    Added basic logic for generating questions based on the percentages

--- reelo/questionGenerator/views.py
+++ reelo/questionGenerator/views.py
@@ -1,30 +1,59 @@
 from django.shortcuts import render, HttpResponse
-import requests, json, time
 from .models import QuestionsDB
+
+# Libraries used for the purpose of the 
+# import requests, json, time
 
 # Create your views here.
 def home(request):
+    total_ques = 50
 
-    for diff in ['easy', 'hard', 'medium']:
-        time.sleep(10)
-        # Sports --> 21; Geography --> 22; Science and Nature --> 17; General Knowledge --> 9
-        # Science Computers --> 18; Maths --> 19; Animals --> 27
-        for category in [21, 22, 17, 9, 18, 19, 27]:
-            time.sleep(10)
+    easy_ques = 27    
+    medium_ques = 33
+    hard_ques = 40
 
-            done = diff + ' ' + str(category)
+    print(f"Easy questions will be {round((total_ques * easy_ques) / 100)} questions")
+    print(f"Easy questions will be {round((total_ques * medium_ques) / 100)} questions")
+    print(f"Easy questions will be {round((total_ques * hard_ques) / 100)} questions")
+
+    question_list = []
+
+    for diff in ['easy', 'medium','hard']:
+        question_list.extend(QuestionsDB.objects.filter(difficulty = diff).order_by('?')[:3])
+
+
+    print(question_list)
+    
+    content = ''
+
+    for obj in question_list:
+        content += obj.question
+
+    return HttpResponse(f"This is the page + {content}")
+
+
+
+# Script for populating the db
+
+    # for diff in ['easy', 'hard', 'medium']:
+    #     time.sleep(10)
+    #     # Sports --> 21; Geography --> 22; Science and Nature --> 17; General Knowledge --> 9
+    #     # Science Computers --> 18; Maths --> 19; Animals --> 27
+    #     for category in [21, 22, 17, 9, 18, 19, 27]:
+    #         time.sleep(10)
+
+    #         done = diff + ' ' + str(category)
             
-            base_url = f'https://opentdb.com/api.php?amount=50&category={category}&difficulty={diff}'
+    #         base_url = f'https://opentdb.com/api.php?amount=50&category={category}&difficulty={diff}'
 
-            response = json.loads(requests.get(base_url).text)['results']
+    #         response = json.loads(requests.get(base_url).text)['results']
 
-            # Getting the reponse from the api so that it can be directly run in the django and then can be used 
-            # to fill up the db
-            for quest in response:
-                QuestionsDB(question = quest['question'],category = quest['category'], correctAns = quest['correct_answer'], incorrectAns = quest['incorrect_answers'],
-                            difficulty = quest['difficulty'], type = quest['type']).save()
+    #         # Getting the reponse from the api so that it can be directly run in the django and then can be used 
+    #         # to fill up the db
+    #         for quest in response:
+    #             QuestionsDB(question = quest['question'],category = quest['category'], correctAns = quest['correct_answer'], incorrectAns = quest['incorrect_answers'],
+    #                         difficulty = quest['difficulty'], type = quest['type']).save()
             
-            print(done)
+    #         print(done)
 
     # https://opentdb.com/api_config.php
-    return HttpResponse("This is the page")
